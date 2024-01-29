cea8d7162d54ec5c012377b63e671cd5c4cb6e30
harrison <harrisonshelnutt@gmail.com>
2024-01-29 10:01:56 -0700

    added command descriptions to show in CLI when user runs "help" command

--- src/commands.rs
+++ src/commands.rs
@@ -3,10 +3,17 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(no_binary_name = true, override_usage = "set|delete|show|exit|help [args]")]
 pub enum Command {
+    /// Add a new item or update an existing one by name
     #[clap(override_usage = "set <ITEM_NAME> <ITEM_VALUE>")]
     SET { item_name: String, item_value: String },
+
+    /// Delete an existing item by name
     #[clap(override_usage = "delete <ITEM_NAME>")]
     DELETE { item_name: String },
+
+    /// Show all items
     SHOW,
+
+    /// Exit SecretStore
     EXIT,
 }
