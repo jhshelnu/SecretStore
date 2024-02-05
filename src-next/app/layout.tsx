c38812ff583d9b24229b11fa15a102e2270df666
harrison <harrisonshelnutt@gmail.com>
2024-02-05 15:01:42 -0700

    initialized tauri front end, started load-from-file UI

---
+++ src-next/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
+import {CssVarsProvider} from "@mui/joy";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+    <head>
+      <title>SecretStore</title>
+      <meta name="viewport" content="initial-scale=1, width=device-width"/>
+    </head>
+    <body className={inter.className}>
+      <CssVarsProvider defaultMode="dark">{children}</CssVarsProvider>
+    </body>
+    </html>
+  );
+}
