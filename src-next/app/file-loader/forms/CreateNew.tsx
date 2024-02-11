657c579ca1a8c44c160c22bed6183a94b4e59ac6
harrison <harrisonshelnutt@gmail.com>
2024-02-11 15:46:30 -0700

    css fixes, moved "from existing" screen to submenu on home screen

---
+++ src-next/app/file-loader/forms/CreateNew.tsx
@@ -0,0 +1,7 @@
+import {Typography} from "@mui/joy";
+
+export default function CreateNew() {
+    return (
+        <Typography level="title-md">TODO!</Typography>
+    )
+}
--- src-next/app/file-loader/page.tsx
+++ src-next/app/file-loader/page.tsx
@@ -1,20 +1,42 @@
 'use client'
 
-import {Box, Button, Typography} from "@mui/joy"
+import {Box, Button, ButtonGroup, Typography} from "@mui/joy"
 import {useRouter} from "next/navigation";
+import {useState} from "react";
+import FromExisting from "@/app/file-loader/forms/FromExisting";
+import CreateNew from "@/app/file-loader/forms/CreateNew";
+
+enum FileLoadType {
+    NEW, EXISTING
+}
 
 export default function FileLoader() {
-    const router = useRouter();
+    const [selectedFileLoadType, setSelectedFileLoadType] = useState<FileLoadType | null>(null);
 
     return (
         <Box display="flex" flexDirection="column" alignItems="center" gap="1em">
             <Typography level="title-lg" fontSize="50px">SecretStore</Typography>
-            <Button
-                component="label"
-                variant="soft"
-                onClick={() => router.replace("/file-loader/from-existing")}>
-                Open existing Secretsfile
-            </Button>
+
+            <ButtonGroup
+                orientation="horizontal"
+                sx={{ '--ButtonGroup-radius': '25px'}}
+            >
+                <Button
+                    className={selectedFileLoadType == FileLoadType.NEW ? "bg-color-selected" : "bg-color-hoverable"}
+                    onClick={() => setSelectedFileLoadType(FileLoadType.NEW)}
+                >
+                    <Typography level="body-md">Create New File</Typography>
+                </Button>
+                <Button
+                    className={selectedFileLoadType == FileLoadType.EXISTING ? "bg-color-selected" : "bg-color-hoverable"}
+                    onClick={() => setSelectedFileLoadType(FileLoadType.EXISTING)}
+                >
+                    <Typography level="body-md">Open existing File</Typography>
+                </Button>
+            </ButtonGroup>
+
+            {selectedFileLoadType == FileLoadType.NEW && <CreateNew />}
+            {selectedFileLoadType == FileLoadType.EXISTING && <FromExisting />}
         </Box>
     )
 }
